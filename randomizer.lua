28f1af26dd62ab1c02ab7d3309ce227606294556
MizaruWareCC <129681170+MizaruWareCC@users.noreply.github.com>
2025-02-08 12:15:03 +0300

    Update randomizer.lua

--- randomizer.lua
+++ randomizer.lua
@@ -1,9 +1,9 @@
 utils.reg_tool("Randomizer", function() 
   local ans = input("Random number or string: ")
   if ans:startswith("n") then
-      print(random.number())
+      print(random.number(tonumber(input("Enter minimum value: ")), tonumber(input("Enter maximum value: "))))
   elseif ans:startswith("s") then
-      print(random.string())
+      print(random.string(tonumber(input("Enter length: "))))
   else
     print("Unknown option!")
   end
