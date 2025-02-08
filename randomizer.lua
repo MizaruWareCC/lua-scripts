ac63953ff6dbab10f5d9d22fabe69ce354bf5eb6
MizaruWareCC <129681170+MizaruWareCC@users.noreply.github.com>
2025-02-08 12:05:57 +0300

    Update randomizer.lua

--- randomizer.lua
+++ randomizer.lua
@@ -1,3 +1,4 @@
-reg_tool("Randomizer", function() 
-  print("Blah")
+utils.reg_tool("Randomizer", function() 
+  local ans = input("Random number or string: ")
+  if ans:startswith("n") then print(random.number()) elseif ans:startswith("s") then print(random.string())) else print("Unknown option!") end
 end)
