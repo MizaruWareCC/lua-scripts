d3414283ecf57c1343b3947a01e7e501421c886c
MizaruWareCC <129681170+MizaruWareCC@users.noreply.github.com>
2025-02-11 18:02:49 +0300

    Update file_downloader.lua

--- file_downloader.lua
+++ file_downloader.lua
@@ -3,7 +3,11 @@ utils.reg_tool("File downloader", function()
     local http = HttpClient.new()
     print("Please, wait a bit...")
     local resp = http:get(site)
-    local f = File.new(Directory.new(), "downloaded.txt")
-    f:write(resp.text)
-    print("Saved info to downloaded.txt it current directory!")
+    if resp.status_code ~= 200 or resp.status_code ~= 204 then
+        print("Failed to download! Check internet connection/url.")
+    else
+        local f = File.new(Directory.new(), "downloaded.txt")
+        f:write(resp.text)
+        print("Saved info to downloaded.txt it current directory!")
+    end
 end)
