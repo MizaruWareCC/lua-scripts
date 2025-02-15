4f05bf23659b141e367ad1aa408cc60bea16fbb1
MizaruWareCC <129681170+MizaruWareCC@users.noreply.github.com>
2025-02-15 20:20:23 +0300

    Update file_downloader.lua

--- file_downloader.lua
+++ file_downloader.lua
@@ -3,9 +3,5 @@ register_tool("Show site response", function()
     local http = HttpClient.new()
     print("Please, wait a bit...")
     local resp = http:get(site)
-    if resp.status_code ~= 200 or resp.status_code ~= 204 then
-        print("Failed to download! Check internet connection/url. Orresponse wasn't 200/204")
-    else
-        print(resp.text)
-    end
+    print(resp.text)
 end)
